1208d332af04a327df3c402b0b05306ac5077f6b
SeemsLikeHappiness <antonkiselev144@gmail.com>
2025-10-01 19:47:04 +0500

    Add files via upload

---
+++ homework2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	float number;
+	cout << "Enter number: ";
+	cin >> number;
+	float squareNumber;
+	squareNumber = pow(number, 2);
+	cout << "Square of the number: "<<squareNumber << "\n";
+	float cubeNumber;
+	cubeNumber = pow(number, 3);
+	cout << "Cube of the number: " << cubeNumber;
+}
+
