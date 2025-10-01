b669abe6fb8febe97ebfeca2a9340019b0e69d42
SeemsLikeHappiness <antonkiselev144@gmail.com>
2025-10-01 18:01:37 +0500

    Add files via upload

---
+++ homework1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+// Программа, выводящая в консоль персональное приветствие
+
+int main()
+{
+    char userName[30];
+    cout << "Enter your name\n";
+    cin >> userName;
+    cout << "Hello " << userName;
+}
