d36accf74baaade2e4d838ef4b049d2feb6d51d6
SeemsLikeHappiness <antonkiselev144@gmail.com>
2025-10-01 20:02:47 +0500

    Add files via upload

---
+++ homework3.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter the amount in rubles: ";
+    float amount;
+    cin >> amount;
+    cout << "In dollars: " << amount * 85 << "\n";
+    cout << "In euros: " << amount * 100 << "\n";
+}
