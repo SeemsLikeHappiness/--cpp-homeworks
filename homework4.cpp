a92612ab60fd5d8ade5f5028d475659024b68960
SeemsLikeHappiness <antonkiselev144@gmail.com>
2025-10-10 19:26:20 +0500

    Add files via upload

---
+++ homework4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double number;
+
+    cout << "Enter double number: ";
+    cin >> number;
+
+    cout << "As double: " << number << endl;
+
+    int intNumber = static_cast<int>(number);
+    cout << "As int: " << intNumber << endl;
+
+    bool boolNumber = static_cast<bool>(number);
+    cout << "Is the number not equal to 0? " << boolalpha << boolNumber << endl;
+
+    return 0;
+}
