0fcb3172e279178549ce0a9f3e6c4c9f50f96b4a
SeemsLikeHappiness <antonkiselev144@gmail.com>
2025-12-13 00:18:08 +0500

    Add files via upload

---
+++ homework12.1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class Rectangle {
+private:
+    double width;
+    double height;
+
+public:
+    Rectangle() : width(0.0), height(0.0) {}
+
+    Rectangle(double w, double h) : width(w), height(h) {}
+
+    double getWidth() const { return width; }
+    double getHeight() const { return height; }
+
+    void setWidth(double w) { width = w; }
+    void setHeight(double h) { height = h; }
+
+    void show() const {
+        cout << fixed << setprecision(1);
+        cout << "Ширина: " << width << endl;
+        cout << "Высота: " << height << endl;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    Rectangle rect1(4.0, 3.0);
+    rect1.show();
+    cout << endl;
+    return 0;
+}
---
+++ homework12.3.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class Rectangle {
+private:
+    double width;
+    double height;
+
+public:
+    Rectangle() : width(0.0), height(0.0) {}
+
+    Rectangle(double w, double h) : width(w), height(h) {}
+
+    double getWidth() const { return width; }
+    double getHeight() const { return height; }
+
+    void setWidth(double w) { width = w; }
+    void setHeight(double h) { height = h; }
+
+    void show() const {
+        cout << fixed << setprecision(1);
+        cout << "Ширина: " << width << endl;
+        cout << "Высота: " << height << endl;
+    }
+
+    double area() const {
+        return width * height;
+    }
+
+    void showExtended() const {
+        show();
+        cout << "Площадь: " << area() << endl;
+    }
+
+    void scale(double coeff) {
+        if (coeff > 0) {
+            width *= coeff;
+            height *= coeff;
+        }
+        else {
+            cout << "Ошибка: коэффициент должен быть положительным!" << endl;
+        }
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    Rectangle rect(4.0, 3.0);
+    rect.scale(2.0);
+    rect.showExtended();
+    return 0;
+}
